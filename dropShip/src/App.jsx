66627c06b132ce28d6f48f1b62d11f7445646d0d
Hleb <hleb>
2025-06-25 13:33:00 +0200

    Create a default route

--- dropShip/src/App.jsx
+++ dropShip/src/App.jsx
@@ -1,5 +1,5 @@
 import NavBar from "./components/layout/NavBar.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Men from "./pages/Men.jsx";
 import Women from "./pages/Women.jsx";
@@ -11,6 +11,7 @@ function App() {
     <div>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/men" element={<Men />}></Route>
         <Route path="/women" element={<Women />}></Route>
