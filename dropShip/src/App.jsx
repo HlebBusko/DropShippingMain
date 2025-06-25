976074ff569921a3ba7a389d82651c92f3bcc121
Hleb <hleb>
2025-06-25 13:02:59 +0200

    Define working routes

--- dropShip/src/App.jsx
+++ dropShip/src/App.jsx
@@ -1,5 +1,5 @@
 import NavBar from "./components/layout/NavBar.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Men from "./pages/Men.jsx";
 import Women from "./pages/Women.jsx";
@@ -10,6 +10,13 @@ function App() {
   return (
     <div>
       <NavBar />
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/men" element={<Men />}></Route>
+        <Route path="/women" element={<Women />}></Route>
+        <Route path="/kids" element={<Kids />}></Route>
+        <Route path="/equipment" element={<Equipment />}></Route>
+      </Routes>
     </div>
   );
 }
